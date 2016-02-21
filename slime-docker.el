5cc2f6e3156aa9a94fc71931f8a036b14e440d30
Eric Timmons <etimmons@mit.edu>
2016-02-21 17:33:48 -0500

    Version bump to get docker-machine integration.

--- slime-docker.el
+++ slime-docker.el
@@ -3,7 +3,7 @@
 ;; URL: https://github.com/daewok/slime-docker
 ;; Package-Requires: ((emacs "24") (slime "2.16") (docker-tramp "0.1") (cl-lib "0.5"))
 ;; Keywords: docker, lisp, slime
-;; Version: 0.2
+;; Version: 0.3
 
 
 ;;; License:
