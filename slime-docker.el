797594cf41fecaa77d502fa973808458903ae9e2
Eric Timmons <etimmons@mit.edu>
2021-01-22 21:34:56 -0500

    Update copyright dates

--- slime-docker.el
+++ slime-docker.el
@@ -10,7 +10,7 @@
 
 ;;  GPLv2+
 
-;;  Copyright (c) 2016 Eric Timmons
+;;  Copyright (c) 2016-2021 Eric Timmons
 
 ;;  This program is free software; you can redistribute it and/or modify it
 ;;  under the terms of the GNU General Public License as published by the Free
